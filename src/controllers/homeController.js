2de153c52e51690d5be2b0cf6faef09cd9a2ea6c
MSI\User <zethuynh123@gmail.com>
2023-09-29 16:52:46 +0700

    first commit

---
+++ src/controllers/homeController.js
@@ -0,0 +1,23 @@
+import db from "../models/index";
+
+let getHomePage = async (req, res) => {
+  try {
+    let data = await db.User.findAll();
+    return res.render("homepage.ejs", { data: JSON.stringify(data) });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+let getAboutPage = (req, res) => {
+  return res.render("test/about.ejs");
+};
+
+// object: {
+//     key: '',
+//     value: ''
+// }
+module.exports = {
+  getHomePage: getHomePage,
+  getAboutPage: getAboutPage,
+};
