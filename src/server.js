42145f7033b961fb45b581ed6134c7567b617e5f
MSI\User <zethuynh123@gmail.com>
2023-10-14 22:27:06 +0700

    save image type blob on DB success

--- src/server.js
+++ src/server.js
@@ -9,10 +9,10 @@ require("dotenv").config();
 
 let app = express();
 app.use(cors({ credentials: true, origin: true }));
-//config app
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+//config app
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
 viewEngine(app);
 initWebRoutes(app);
